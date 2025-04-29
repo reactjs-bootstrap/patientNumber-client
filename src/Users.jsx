4aadf899faab7b19c28239e2743a5869c908c51d
reactjs-bootstrap <147966324+reactjs-bootstrap@users.noreply.github.com>
2025-04-29 20:23:14 +0530

    Update Users.jsx

--- src/Users.jsx
+++ src/Users.jsx
@@ -116,8 +116,8 @@ const Users = () => {
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
+                        width="20"
+                        height="20"
                         viewBox="0 0 24 24"
                         color="green"
                       >
@@ -135,8 +135,8 @@ const Users = () => {
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
+                        width="20"
+                        height="20"
                         viewBox="0 0 40 40"
                         color="red"
                       >
