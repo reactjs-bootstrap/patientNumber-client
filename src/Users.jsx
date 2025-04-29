731b71644c7ef92259ac5640d9afd03dee77b1cf
reactjs-bootstrap <147966324+reactjs-bootstrap@users.noreply.github.com>
2025-04-29 17:57:21 +0530

    Update Users.jsx

--- src/Users.jsx
+++ src/Users.jsx
@@ -45,11 +45,14 @@ const Users = () => {
   return (
     <div className="d-flex justify-content-center align-items-start">
       <div className="bg-white rounded p-3">
-        <Link to="/create" className="btn btn-success">
+        <Link to="/create" className="btn btn-success btn-sm">
           Add +
         </Link>
-        <button className="btn btn-info ms-2" onClick={() => setSort(!sort)}>
-          {sort == false ? "A-Z" : "Z-A"}
+        <button
+          className="btn btn-info btn-sm ms-2"
+          onClick={() => setSort(!sort)}
+        >
+          {sort == true ? "A-Z" : "Z-A"}
         </button>
         <br />
         {/* <br />
@@ -64,37 +67,63 @@ const Users = () => {
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         /> */}
-        <table className="table table-sm">
-          <thead>
+        <table className="table table-sm table-bordered mt-2 mb-2">
+          <thead className="table-secondary">
             <tr>
-              <th className="p-2">Sr. No.</th>
-              <th className="p-2">Room No.</th>
-              <th className="p-2">Patient Name</th>
-              <th className="p-2">Ref</th>
-              <th className="p-2">Action</th>
+              <th className="pe-2 ps-2">Sr. No.</th>
+              <th className="pe-2 ps-2">Room No.</th>
+              <th className="pe-2 ps-2">Patient Name</th>
+              <th className="pe-2 ps-2">Ref</th>
+              <th className="pe-2 ps-2">Action</th>
             </tr>
           </thead>
           <tbody>
             {currentData.map((user, index) => {
               return (
                 <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{user.room}</td>
+                  <td className="text-center">{index + 1}</td>
+                  <td className="text-center">{user.room}</td>
                   <td>{user.name}</td>
                   <td>{user.ref == true ? "Yes" : null}</td>
                   <td>
                     <Link
                       to={`/update/${user._id}`}
-                      className="btn btn-success btn-sm"
+                      className="btn btn-sm  p-0"
                     >
-                      Update
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        color="green"
+                      >
+                        <rect width="24" height="24" fill="none" />
+                        <path
+                          fill="currentColor"
+                          d="M5 3c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7H5V5h7V3zm12.78 1a.7.7 0 0 0-.48.2l-1.22 1.21l2.5 2.5L19.8 6.7c.26-.26.26-.7 0-.95L18.25 4.2c-.13-.13-.3-.2-.47-.2m-2.41 2.12L8 13.5V16h2.5l7.37-7.38z"
+                        />
+                      </svg>
                     </Link>
                     <Link
                       to="/"
-                      className="btn btn-success btn-sm ms-2"
+                      className="btn  btn-sm p-0 ms-2"
                       onClick={() => handleDelete(user._id)}
                     >
-                      Delete
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 40 40"
+                        color="red"
+                      >
+                        <rect width="40" height="40" fill="none" />
+                        <path
+                          fill="currentColor"
+                          d="M21.499 19.994L32.755 8.727a1.064 1.064 0 0 0-.001-1.502c-.398-.396-1.099-.398-1.501.002L20 18.494L8.743 7.224c-.4-.395-1.101-.393-1.499.002a1.05 1.05 0 0 0-.309.751c0 .284.11.55.309.747L18.5 19.993L7.245 31.263a1.064 1.064 0 0 0 .003 1.503c.193.191.466.301.748.301h.006c.283-.001.556-.112.745-.305L20 21.495l11.257 11.27c.199.198.465.308.747.308a1.06 1.06 0 0 0 1.061-1.061c0-.283-.11-.55-.31-.747z"
+                          stroke-width="1"
+                          stroke="currentColor"
+                        />
+                      </svg>
                     </Link>
                   </td>
                 </tr>
